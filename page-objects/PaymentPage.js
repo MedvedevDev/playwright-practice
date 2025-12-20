00c0df39fc94ae7dba7fb51f0536b044aa480aeb
MedvedevDev <a95.medvedev@gmail.com>
2025-12-20 11:06:10 +0100

    Testing that discount is applied

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -7,6 +7,16 @@ export class PaymentPage {
       .frameLocator('[data-qa="active-discount-container"]')
       .locator('[data-qa="discount-code"]');
     this.discountInput = page.getByPlaceholder("Discount code");
+    this.totalValue = page.locator('[data-qa="total-value"]');
+    this.discountedValue = page.locator(
+      '[data-qa="total-with-discount-value"]'
+    );
+    this.discountMessage = page.locator('[data-qa="discount-active-message"]');
+
+    // Buttons
+    this.activateDiscountButton = page.locator(
+      '[data-qa="submit-discount-button"]'
+    );
   }
 
   activateDiscount = async () => {
@@ -15,15 +25,32 @@ export class PaymentPage {
     const code = await this.discountCode.innerText();
 
     // fill out the discount input
-    // await this.discountInput.fill(code);
-    // await expect(this.discountInput).toHaveValue(code);
+    await this.discountInput.fill(code);
+    await expect(this.discountInput).toHaveValue(code);
 
     // slow typing the discount input
     // ---1) With keyboard typing
     //await this.discountInput.focus();
     //await this.page.keyboard.type(code, { delay: 1000 });
     // ---2) With pressSequentially method
-    await this.discountInput.pressSequentially(code, { delay: 1000 });
-    expect(await this.discountInput.inputValue()).toBe(code);
+    // await this.discountInput.pressSequentially(code, { delay: 1000 });
+    // expect(await this.discountInput.inputValue()).toBe(code);
+
+    await this.activateDiscountButton.click();
+
+    // Check that "Discount activated" text is displayed
+    await this.discountMessage.waitFor();
+    // Check that there is now a discounted price total showing
+    await this.discountedValue.waitFor();
+    // Check that the discounted price total is smaller than the regular price
+    const totalValueText = await this.totalValue.innerText();
+    const totalValueClear = totalValueText.replace("$", "");
+    const totalValueNumber = parseInt(totalValueClear);
+
+    const discountedValueText = await this.discountedValue.innerText();
+    const discountedValueClear = discountedValueText.replace("$", "");
+    const discountedValueNumber = parseInt(discountedValueClear);
+
+    expect(discountedValueNumber).toBeLessThan(totalValueNumber);
   };
 }
