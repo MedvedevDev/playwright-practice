0d1d99caf0f96a20af0c7b38c07272116d0075d8
MedvedevDev <a95.medvedev@gmail.com>
2026-01-27 22:49:31 +0100

    grouping tests

--- new-tests/tests/first-test.spec.ts
+++ new-tests/tests/first-test.spec.ts
@@ -1,7 +1,19 @@
 import { test } from "@playwright/test";
 
-test("the first test", async ({ page }) => {
+test.beforeEach("go to localhost", async ({ page }) => {
   await page.goto("http://localhost:4200");
-  await page.getByText("Forms").click();
-  await page.getByText("Form Layouts").click();
+});
+
+test.describe("firms suite", () => {
+  test.beforeEach("click forms", async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+
+  test("test 1", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("test 2", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
